45b094accf5b539fe4d74369bb04711efeb351fa
buehmann <buehmann@e872857c-5b07-0410-aa81-e3586f851bf3>
2005-11-19 18:00:48 +0000

    Preview-Release

---
+++ fritzbox/callmonitor/tags/callmonitor-0.1-ds/base/usr/lib/callmonitor/phonebook.sh
@@ -0,0 +1,25 @@
+. "${CALLMONITOR_LIBDIR}/net.sh"
+
+# resolve numbers to names and addresses (www.dasoertliche.de); the number is
+# used as given (should be normalized beforehand)
+reverse_lookup() {
+	local NUMBER="$1"
+	case "$NUMBER" in
+		00*|[1-9]*) return;
+	esac
+	getmsg -w 5 www.dasoertliche.de "$NUMBER" \
+	-t '/DB4Web/es/oetb2suche/home.htm?main=Antwort&s=2&kw_invers=%s' |
+	sed -e '/<a class="blb" href="home.htm/!d' \
+	-e 's#<br>#, #g' -e 's#<[^>]*># #g' \
+	-e 's#[ 	][ 	]*# #g' -e 's#^ ##' -e 's# $##' -e 's# ,#,#'
+}
+# normalize phone numbers
+normalize_number() {
+	local NUMBER="$1"
+	case $NUMBER in
+		0049*) NUMBER="0${NUMBER#0049}" ;;
+		0*) ;;
+		*) NUMBER="${CALLMONITOR_OKZ}${NUMBER}" ;; 
+	esac
+	echo "$NUMBER"
+}
