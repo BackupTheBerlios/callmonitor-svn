6ed55b406487b49e1f517d2c5d5fd1acb65417aa
buehmann <buehmann@e872857c-5b07-0410-aa81-e3586f851bf3>
2006-06-24 16:11:41 +0000

    Release 1.4

---
+++ fritzbox/callmonitor/tags/callmonitor-1.4/base/usr/lib/callmonitor/actions.d/messages.sh
@@ -0,0 +1,91 @@
+##
+## Callmonitor for Fritz!Box (callmonitor)
+## 
+## Copyright (C) 2005--2006  Andreas Bühmann <buehmann@users.berlios.de>
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+## 
+## http://developer.berlios.de/projects/callmonitor/
+##
+
+## Listener types and common utilities; separate function for each type
+## of listener. Put your own into $CALLMONITOR_LIBDIR/actions.local.d/*.sh
+## 
+## These environment variables are set by callmonitor before calling
+## calling a listener:
+##  EVENT	event {in,out}:{request,cancel,connect,disconnect}
+##  SOURCE	source number
+##  SOURCE_NAME source name
+##  DEST	destination number
+##  DEST_NAME	destination name
+
+require net
+require message
+
+## get matching IPs from multid.leases and execute a command for each of them
+## example: for_leases 192.168.10. dboxpopup "Ring!"
+for_leases() {
+    local pattern=$1 command=$2 lease mac ip rest
+    shift 2
+    while read -r lease mac ip rest; do
+	case $ip in *$pattern*) "$command" "$ip" "$@" & ;; esac
+    done < /var/flash/multid.leases
+}
+
+## simple *box listeners
+dboxpopup() {
+    getmsg -t "/control/message?popup=%s" -d default_dboxpopup "$@"
+}
+dboxmessage() {
+    getmsg -t "/control/message?nmsg=%s" -d default_dboxmessage "$@"
+}
+default_dboxpopup() { default_dbox; }
+default_dboxmessage() { default_dbox; }
+default_dbox() {
+    default_message | latin1_utf8 | sed -e 's/,[[:space:]]\+/\n/g'
+}
+dreammessage() {
+    getmsg -t "/cgi-bin/xmessage?timeout=${DREAM_TIMEOUT:-10}&caption=$(urlprintfencode "${DREAM_CAPTION:-$(lang
+    de:"Telefonanruf" en:"Phone call")}")&charset=latin1&icon=${DREAM_ICON:-1}&body=%s" -d default_dreammessage "$@"
+}
+default_dreammessage() { default_message; }
+
+## Usage: yac [OPTION]... [MESSAGE]
+## Send a message to a yac listener (Yet Another Caller ID Program)
+yac() {
+    rawmsg -p 10629 -t "%s\0" -d default_yac "$@"
+}
+default_yac() {
+    echo "@CALL$SOURCE_NAME~$SOURCE"
+}
+
+## Usage: vdr [OPTION]... [MESSAGE]
+## Send a message to a VDR (Video Disk Recorder, http://www.cadsoft.de/vdr/)
+vdr() {
+    rawmsg -p 2001 -t "MESG %s\nQUIT\n" -d default_vdr "$@"
+}
+default_vdr() {
+    echo "$(lang de:"Anruf" en:"Call")${SOURCE:+" $SOURCE"}${SOURCE_NAME:+" - $SOURCE_NAME"}"
+}
+
+xboxmessage() {
+    getmsg -t "/xbmcCmds/xbmcHttp?command=ExecBuiltIn&parameter=XBMC.Notification($(urlprintfencode "${XBOX_CAPTION:-$(lang de:"Telefonanruf" en:"Phone call")}"),%s)" -d default_xboxmessage "$@"
+}
+__xboxmessage() {
+    default_xboxmessage | tr "," ";"
+}
+default_xboxmessage() {
+    default_message "" 2
+}
---
+++ fritzbox/callmonitor/tags/callmonitor-1.4/mod/dsmod/root/usr/lib/callmonitor/applets/callmonitor.cgi.sh
@@ -0,0 +1,196 @@
+##
+## Callmonitor for Fritz!Box (callmonitor)
+## 
+## Copyright (C) 2005--2006  Andreas Bühmann <buehmann@users.berlios.de>
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+## 
+## http://developer.berlios.de/projects/callmonitor/
+##
+require cgi
+require if_jfritz_status
+
+CHECKED=' checked'
+SELECTED=' selected'
+
+auto_chk='' man_chk=''
+case $CALLMONITOR_ENABLED in
+    yes) auto_chk=$CHECKED ;;
+    *) man_chk=$CHECKED ;;
+esac
+
+debug_chk=''
+case $CALLMONITOR_DEBUG in
+    yes) debug_chk=$CHECKED ;;
+esac
+
+reverse_chk=''
+case $CALLMONITOR_REVERSE in
+    yes) reverse_chk=$CHECKED
+esac
+
+no_sel='' trans_sel='' pers_sel=''
+case "$CALLMONITOR_REVERSE_CACHE" in
+    no) no_sel=$SELECTED ;;
+    transient) trans_sel=$SELECTED ;;
+    persistent) pers_sel=$SELECTED ;;
+esac
+
+oert_sel='' werdran_sel='' invers_sel=''
+case "$CALLMONITOR_REVERSE_PROVIDER" in
+    dasoertliche) oert_sel=$SELECTED ;;
+    weristdran) werdran_sel=$SELECTED ;;
+    inverssuche) invers_sel=$SELECTED ;;
+esac
+
+SYSLOG='$(lang de:"System-Log" en:"system log")'
+if has_package syslogd; then
+    SYSLOG="<a href='pkgconf.cgi?pkg=syslogd'>$SYSLOG</a>"
+fi
+
+CALLMONITOR_VERSION="$(cat /mod/etc/default.callmonitor/.version)"
+
+## cat << EOF
+## <p style="text-align: right;">
+## <a href="$CALLMONITOR_FORUM_URL">Forum</a>, 
+## <a href="$CALLMONITOR_DOC_URL">$(lang de:"Hilfe" en:"Help")</a>
+## </p>
+## EOF
+
+sec_begin '$(lang de:"Starttyp" en:"Startup type")'
+
+cat << EOF
+<p>
+    <input type="radio" name="enabled" value="yes"$auto_chk id="e1">
+    <label for="e1">$(lang de:"Automatisch" en:"Automatic")</label>
+    <input type="radio" name="enabled" value="no"$man_chk id="e2">
+    <label for="e2">$(lang de:"Manuell" en:"Manual")</label>
+</p>
+<p>
+    <input type="hidden" name="debug" value="no">
+    <input type="checkbox" name="debug" value="yes"$debug_chk id="d1">
+    <label for="d1">$(lang 
+	de:"mit Debug-Ausgaben" 
+	en:"with debugging output"
+    )</label> ($(lang de:"ins" en:"into the") $SYSLOG)
+</p>
+EOF
+
+sec_end
+sec_begin '$(lang de:"Status" en:"Status")'
+
+cat << EOF
+<ul>
+    <li>Version: $CALLMONITOR_VERSION</li>
+EOF
+if ! _j_is_up; then
+    cat << EOF
+    <li><strong style="color: red">$(lang 
+	de:"Die CallMonitor-Schnittstelle (Port 1012) ist nicht aktiv."
+	en:"The CallMonitor interface (port 1012) is not active."
+    )</strong>
+    $(lang
+	de:"Sie wird zum Betrieb des Callmonitors benötigt."
+	en:"It is required for Callmonitor's operation."
+    ) [<a href="extras.cgi/callmonitor/exec?jfritz=on">$(lang
+	de:"Einschalten" en:"Enable"
+    )</a>]
+    </li>
+EOF
+else
+    cat << EOF
+    <li>$(lang
+	de:"Die CallMonitor-Schnittstelle (Port 1012) ist aktiviert."
+	en:"The CallMonitor interface (port 1012) is active."
+    ) [<a href="extras.cgi/callmonitor/exec?jfritz=off">$(lang
+	de:"Ausschalten" en:"Disable"
+    )</a>]
+EOF
+fi
+cat << EOF
+</ul>
+EOF
+
+sec_end
+sec_begin '$(lang de:"Aktionen bei Anruf" en:"Actions upon calls")'
+
+cat << EOF
+<ul>
+    <li><a href="/cgi-bin/file.cgi?id=listeners">$(lang
+	de:"Listeners bearbeiten" en:"Edit Listeners")</a></li>
+    <li><a href="/cgi-bin/extras.cgi/callmonitor/testcall">$(lang
+	de:"Testanruf" en:"Test call")</a></li>
+</ul>
+EOF
+
+sec_end
+sec_begin '$(lang de:"Rückwärtssuche" en:"Reverse lookup")'
+
+cat << EOF
+<p>
+    <input type="hidden" name="reverse" value="no">
+    <input type="checkbox" name="reverse" value="yes"$reverse_chk id="r4">
+    <label title="$(lang
+	de:"Rufnummern wenn möglich in Namen auflösen"
+	en:"Resolve numbers to names if possible"
+    )" for="r4">$(lang
+	de:"Rückwärtssuche durchführen"
+	en:"Perform reverse lookup"
+    )</label>
+    <label for="provider">$(lang de:"bei" en:"at")</label>
+    <select name="reverse_provider" id="provider">
+	<option title="www.dasoertliche.de"
+	    value="dasoertliche"$oert_sel>DasÖrtliche</option>
+	<option title="wer-ist-dran.de"
+	    value="weristdran"$werdran_sel>Wer ist dran? (ABIS)</option>
+	<option title="www.inverssuche.de"
+	    value="inverssuche"$invers_sel>inverssuche.de</option>
+    </select>
+</p>
+<p>
+    <label for="cache">$(lang
+	de:"Suchergebnisse zwischenspeichern?"
+	en:"Cache query results?"
+    )</label>
+    <select name="reverse_cache" id="cache">
+	<option title="$(lang
+	    de:"Keine Speicherung der Namen"
+	    en:"Names are not stored"
+	)" value="no"$no_sel>$(lang de:"Nein" en:"No")</option>
+	<option title="$(lang
+	    de:"Namen gehen bei nächstem Neustart verloren"
+	    en:"Names will be lost at the next reboot"
+	)" value="transient"$trans_sel>$(lang
+	    de:"Flüchtig" en:"Transiently")</option>
+	<option title="$(lang 
+	    de:"Namen werden im Telefonbuch im Flash gespeichert"
+	    en:"Names are stored in the flash memory phone book"
+	)" value="persistent"$pers_sel>$(lang
+	    de:"Dauerhaft" en:"Persistently")</option>
+    </select>
+    [<a href="/cgi-bin/file.cgi?id=callers">$(lang 
+	de:"Callers&nbsp;bearbeiten" en:"Edit&nbsp;Callers")</a>]
+</p>
+<p>
+    <label for="okz">$(lang
+	de:"Vorwahl für lokale Rufnummern"
+	en:"Area code for local numbers"
+    ):</label>
+    <input type="text" name="okz" value="$(httpd -e "$CALLMONITOR_OKZ")"
+	size="5" id="okz">
+</p>
+EOF
+
+sec_end
