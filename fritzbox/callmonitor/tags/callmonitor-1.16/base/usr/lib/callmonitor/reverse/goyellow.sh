cc2df01119e021b80ce1e9f1224c458d274168d5
buehmann <buehmann@e872857c-5b07-0410-aa81-e3586f851bf3>
2009-09-12 06:53:17 +0000

    Release 1.16

---
+++ fritzbox/callmonitor/tags/callmonitor-1.16/base/usr/lib/callmonitor/reverse/goyellow.sh
@@ -0,0 +1,51 @@
+##
+## Callmonitor for Fritz!Box (callmonitor)
+## 
+## Copyright (C) 2005--2008  Andreas Bühmann <buehmann@users.berlios.de>
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+## 
+## http://developer.berlios.de/projects/callmonitor/
+##
+_reverse_goyellow_url() {
+    local number="0${1#${LKZ_PREFIX}49}"
+    URL="http://www.goyellow.de/inverssuche/?TEL=$(urlencode "$number")"
+}
+_reverse_goyellow_request() {
+    local URL=
+    _reverse_goyellow_url "$@" 
+    wget_callmonitor "$URL" -q -O -
+}
+_reverse_goyellow_extract() {
+    sed -n -e '
+	\#haben wir nichts gefunden# {
+	    '"$REVERSE_NA"'
+	}
+	\#<div id="searchResultListing"#,\#<p class="moreInfo"# {
+	    \#<span class="normal"# b name
+	    \#<span class="\(street\|comma\|postcode\|city\)"># H
+	}
+	\#<p class="moreInfo"# {
+	    g
+	    '"$REVERSE_SANITIZE"'
+	    '"$REVERSE_OK"'
+	}
+	b
+	: name
+	s#.*#<rev:name>&</rev:name>#
+	h
+	b
+    ' | utf8_latin1
+}
---
+++ fritzbox/callmonitor/tags/callmonitor-1.16/mod/freetz/root/usr/lib/callmonitor/modules/cgi.sh
@@ -0,0 +1,72 @@
+##
+## Callmonitor for Fritz!Box (callmonitor)
+## 
+## Copyright (C) 2005--2009  Andreas Bühmann <buehmann@users.berlios.de>
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+## 
+## http://developer.berlios.de/projects/callmonitor/
+##
+. /usr/lib/libmodcgi.sh
+
+pre() {
+    echo -n "<pre>"
+    html # stdin
+    echo "</pre>"
+}
+
+config_button() {
+    echo "
+<form class='btn' action='/cgi-bin/pkgconf.cgi' method='get'>
+    <input type='hidden' name='pkg' value='callmonitor'>
+    <div class='btn'><input type='submit' 
+	value='$(lang de:"Konfiguration" en:"Configuration")'></div>
+</form>
+"
+}
+
+_check() {
+    local input=$1
+    local alt key val found=false
+    shift
+    for alt; do
+	key=${alt%%:*}
+	val=${alt#*:}
+	: ${val:=$key}
+	if ! $found; then
+	    case $input in
+		$key) eval "${val}${suffix}=\$checked"; found=true; continue ;;
+	    esac
+	fi
+	eval "${val}_chk="
+    done
+}
+check()  suffix=_chk checked=" checked" _check "$@"
+select() suffix=_sel checked=" selected" _check "$@"
+
+cgi_include() {
+    local path=$1 file
+    case $path in
+	/*) ;;
+	*) path="$CALLMONITOR_LIBDIR/web/$path" ;;
+    esac
+    if [ -d "$path" ]; then
+	for file in $(ls "$path"/*); do
+	    . "$file"
+	done
+    else
+	. "$path"
+    fi
+}
