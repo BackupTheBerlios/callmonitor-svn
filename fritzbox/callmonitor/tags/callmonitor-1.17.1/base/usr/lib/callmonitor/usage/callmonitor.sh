9de40fe13fb387926ee70641dfb303a4fc8b2181
buehmann <buehmann@e872857c-5b07-0410-aa81-e3586f851bf3>
2010-04-09 14:28:31 +0000

    Release 1.17.1

---
+++ fritzbox/callmonitor/tags/callmonitor-1.17.1/base/usr/lib/callmonitor/usage/callmonitor.sh
@@ -0,0 +1,12 @@
+#<
+    cat <<EOF
+Usage:	$APPLET [OPTION]...
+Options:
+    -f		run in foreground
+    -s		stop the callmonitor
+    --version	output version information and exit
+    --debug     log rule matching/executed commands to syslog
+		(and to stderr with -f)
+    --help	show this help
+EOF
+#>
