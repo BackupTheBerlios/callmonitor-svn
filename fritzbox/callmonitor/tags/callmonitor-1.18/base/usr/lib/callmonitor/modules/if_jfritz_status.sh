48afc263300d83dee2ad09651a302dc699ec82f6
buehmann <buehmann@e872857c-5b07-0410-aa81-e3586f851bf3>
2010-05-24 08:05:34 +0000

    Release 1.18

---
+++ fritzbox/callmonitor/tags/callmonitor-1.18/base/usr/lib/callmonitor/modules/if_jfritz_status.sh
@@ -0,0 +1,45 @@
+##
+## Callmonitor for Fritz!Box (callmonitor)
+## 
+## Copyright (C) 2005--2008  Andreas Bühmann <buehmann@users.berlios.de>
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+## 
+## http://developer.berlios.de/projects/callmonitor/
+##
+
+require dial
+
+_j_is_up() {
+    busybox nc "$CALLMONITOR_MON_HOST" "$CALLMONITOR_MON_PORT" < /dev/null > /dev/null 2>&1
+}
+
+_j_dial() {
+    case $CALLMONITOR_MON_HOST in
+	localhost|127.*|"") ;;
+	*) echo "Cannot $2 interface of remote box; please dial $1 manually."; return 1 ;;
+    esac
+    case $CALLMONITOR_MON_PORT in
+	1012) dial "$1" ;;
+	*) echo "Cannot $2 interface at non-standard port."; return 1 ;;
+    esac
+}
+_j_enable() {
+    _j_dial "#96*5*" enable
+}
+
+_j_disable() {
+    _j_dial "#96*4*" disable
+}
